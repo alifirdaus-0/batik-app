3b5d887dc3a8d6d2248b2ee676ebb6b4d912a1d6
alifirdaus-0 <firdausali746@gmail.com>
2025-11-14 20:45:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1001,8 +1001,6 @@ const App = () => {
 };
 
 // Add Leaf icon since it's not in lucide-react
-
-
 const Leaf = (props) => (
   <svg {...props} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
     <path d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z" />
