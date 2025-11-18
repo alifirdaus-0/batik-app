415b6e5e397b9fdee803b90526d1ad3b6f8251bc
alifirdaus-0 <firdausali746@gmail.com>
2025-11-18 08:49:13 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -941,6 +941,8 @@ const App = () => {
         </div>
       </footer>
 
+    {showWelcomePopup && <WelcomePopup />}
+
       {/* Product Modal */}
       {selectedProduct && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
